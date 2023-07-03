dc1b2522880e79c2ce0f9de5fc6b60acbbff97a9
Kedar Gangopadhyay <skg2016@nyu.edu>
2023-07-02 20:43:54 -0400

    minOperations

--- Binary search/minOperations.py
+++ Binary search/minOperations.py
@@ -2,7 +2,7 @@
 # https://leetcode.com/problems/minimum-operations-to-make-numbers-non-positive/
 # https://leetcode.com/problems/minimum-operations-to-make-numbers-non-positive/solutions/3563119/good-binary-search-application-python/
 
-import math
+from math import ceil
 
 
 class Solution:
@@ -28,7 +28,8 @@ class Solution:
                 s += ub_list[i] - lb_list[i]  # Not correct: will not work on nums3
         return m if s <= m else s
 
-    def minOperations(self, nums, x: int, y: int) -> int:
+    # Time limit exceeded
+    def minOperationsGreedy(self, nums, x: int, y: int) -> int:
         """
         nums: List[int]
         x: int
@@ -55,11 +56,36 @@ class Solution:
         isAllNonPositive = all(num <= 0 for num in nums)
         return isAllNonPositive
 
+    def minOperations(self, nums, x, y):
+        """
+        nums: List[int]
+        x: int
+        y: int
+        return: int
+        """
+        low, high = 0, ceil(max(nums) / y)
+        while low < high:
+            mid = (low + high) // 2
+            canFinish = self.canFinish(nums, x, y, mid)
+            if canFinish:
+                high = mid
+            else:
+                low = mid + 1
+        return high
+
+    def canFinish(self, nums, x, y, k):
+        n_ops = 0
+        all_Ys = [num - (y * k) for num in nums]
+        delta = x - y
+        for num in all_Ys:
+            if num > 0:
+                n_ops += ceil(num / delta)
+                if n_ops > k:
+                    return False
+        return True
+
 
 sol = Solution()
 nums1, x1, y1 = [3, 4, 1, 7, 6], 4, 2  # Expected: 3
 nums2, x2, y2 = [1, 2, 1], 2, 1  # Expected: 1
 nums3, x3, y3 = [1, 2, 3, 4, 5], 5, 1  # Expected: 3
-sol.minOperations(nums1, x1, y1)
-sol.minOperations(nums2, x2, y2)
-sol.minOperations(nums3, x3, y3)
