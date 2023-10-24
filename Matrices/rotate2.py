4a4b2c577587c765eef286b3a555780b3df56a90
Kedar Gangopadhyay <skg2016@nyu.edu>
2023-10-23 23:09:28 -0400

    rotate image - note this uses extra space due to NumPy array

---
+++ Matrices/rotate2.py
@@ -0,0 +1,22 @@
+# 48: Rotate Image
+# https://leetcode.com/problems/rotate-image/description/
+
+import numpy as np
+
+
+class Solution:
+    def rotate(self, matrix) -> None:
+        """
+        matrix: List[List[int]]
+        Do not return anything, modify matrix in-place instead.
+        """
+        pass
+
+
+s = Solution()
+
+matrix1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+matrix2 = [[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]]
+matrix3 = [[1]]
+matrix4 = [[1, 2], [3, 4]]
+
