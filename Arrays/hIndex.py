7debf3f71a8e336407736a49ff4e7946c75c5927
Kedar Gangopadhyay <skg2016@nyu.edu>
2023-12-24 12:12:59 -0500

    sorted hIndex

--- Arrays/hIndex.py
+++ Arrays/hIndex.py
@@ -2,6 +2,7 @@
 # https://leetcode.com/problems/h-index/
 
 class Solution:
+    # O(N)
     def hIndex(self, citations) -> int:
         n = len(citations)
         c_cumul = [0] * (n + 1)
@@ -18,3 +19,19 @@ class Solution:
             if accumulator >= i:
                 return i
         return 0
+
+    # O(NlogN)
+    def hIndexSorted(self, citations) -> int:
+        h = len(citations)
+        citations_sorted = sorted(citations)
+        for c in citations_sorted:
+            if c < h:
+                h -= 1
+            else:
+                return h
+        return h
+
+
+s = Solution()
+citations1 = [3, 0, 6, 1, 5]
+citations2 = [1, 3, 1]
